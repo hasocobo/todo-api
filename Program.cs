5f592aae24b78cfcb7efa9f6bd96ce800d927660
Hasan Çoban <hasancoban13579@gmail.com>
2024-06-08 15:32:27 +0300

    Switch Minimal API with Controller-Based API

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-using System.Net.Http.Headers;
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-
-        app.MapGet("/", () => new {name = "Haso", message = "Hello"});
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
